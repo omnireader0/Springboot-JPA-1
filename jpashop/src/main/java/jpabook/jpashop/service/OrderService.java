251c5ccfeb289f22db96e70559e0e8ddd6e4e3b3
omnireader0 <omnireader0@gmail.com>
2022-04-08 16:14:24 +0900

    add : 웹 개발

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -54,9 +56,9 @@ public class OrderService {
     }
 
     /** 주문 검색 */
-    /*
-        public List<Order> findOrders(OrderSearch orderSearch) {
+
+    public List<Order> findOrders(OrderSearch orderSearch) {
         return orderRepository.findAll(orderSearch);
-        }
-	*/
+    }
+
 }
